881ab8dda905a032f1e62be84c439388c576ad22
Ander Armentia <43949411+AnderArmentia@users.noreply.github.com>
2018-11-25 22:11:27 +0100

    Add files via upload

--- EjerciciosArrays/Ejercicio 7/src/ejercicio/pkg7/Ejercicio7.java
+++ EjerciciosArrays/Ejercicio 7/src/ejercicio/pkg7/Ejercicio7.java
@@ -1,21 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio.pkg7;
 
-/**
- *
- * @author Ander
- */
-public class Ejercicio7 {
+import javax.swing.JOptionPane;
 
-    /**
-     * @param args the command line arguments
-     */
+
+public class Ejercicio7 {
+    
+   
     public static void main(String[] args) {
         // TODO code application logic here
+       int [] codigos = {10, 23, 30, 47, 55, 65, 135, 256, 526, 663};
+       int [] cantidades = new int [10];
+       int cantidad;
+    do {   
+    int codigo = Integer.parseInt(JOptionPane.showInputDialog("Introduce un codigo"));
+    int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Introduce una cantidad"));
+
     }
+    while (cantidad >= 10); 
+    String continuar = JOptionPane.showInputDialog("quieres continuar?");
+        if(continuar.equalsIgnoreCase("si")) {
+               int codigo = Integer.parseInt(JOptionPane.showInputDialog("Introduce un codigo"));
+               int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Introduce una cantidad")); 
+        }  
+}
     
 }
+
+        
---
+++ EjerciciosArrays/Ejercicio8/src/ejercicio8/Ejercicio8.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio8;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Ander
+ */
+public class Ejercicio8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [][]  tablas = new int [10][10];
+        int fila = Integer.parseInt(JOptionPane.showInputDialog("Introduce una fila"));
+        int columna = Integer.parseInt(JOptionPane.showInputDialog("Introduce una columna"));
+        int valor = Integer.parseInt(JOptionPane.showInputDialog("Introduce un valor"));
+    }
+    
+}
