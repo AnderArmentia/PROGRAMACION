0fae43c1a4c3155fa15eeeb77e25477a3d7db7bd
Ander Armentia <43949411+AnderArmentia@users.noreply.github.com>
2018-11-29 23:35:54 +0100

    Add files via upload

--- ArrayLists/Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ ArrayLists/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -1,49 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio1;
 
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author 1gdaw11
- */
 public class Ejercicio1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        menu();
         
-        ArrayList <float> valores = new ArrayList();
-        
-            float valor =Float.parseFloat(JOptionPane.showMessageDialog("Introduce un valor"));
-           valores.add("valor");
            
     }          
     public static void menu(){
+            int opcion = 0;
+        do {
         
-       String opcion = JOptionPane.showInputDialog(
-               + "1. Visualizar el valor maximo y el minimo. /n"
-               + "2. Solicitar un numero y buscarlo. Muestra un mensaje indicando si lo has encontrado o no. /n"
-               + "3. Solicitar un numero, buscarlo y borrarlo. Sino se encuentra muestra un mensaje de error. /n"
-               + "4. Convertir el arrayList en un array. /n"
-               + "5. Si no esta vacio, mostrar el numero de elementos que contiene. /n"
-               + "6. Insertar un nuevo elemento por el final. /n"
-               + "7. Insertar un nuevo elemento en la posicion que te indique el usuario./n"
-               + "8. Borrar un elemento de una posicion concreta. /n"
-               + "9. Calcular la suma y la media aritmetica de los valores contenidos. /n"
-               + "10. Finalizar. /n"
-               + "elige una opcion");
-
-      
-    
-        switch (menu){
+         opcion = Integer.parseInt(JOptionPane.showInputDialog(
+                 "1. Visualizar el valor maximo y el minimo. \n" 
+               + "2. Solicitar un numero y buscarlo. Muestra un mensaje indicando si lo has encontrado o no. \n"
+               + "3. Solicitar un numero, buscarlo y borrarlo. Sino se encuentra muestra un mensaje de error. \n"
+               + "4. Convertir el arrayList en un array. \n"
+               + "5. Si no esta vacio, mostrar el numero de elementos que contiene. \n"
+               + "6. Insertar un nuevo elemento por el final. \n"
+               + "7. Insertar un nuevo elemento en la posicion que te indique el usuario.\n"
+               + "8. Borrar un elemento de una posicion concreta. \n"
+               + "9. Calcular la suma y la media aritmetica de los valores contenidos. \n"
+               + "10. Finalizar. \n" 
+               + "\n"      
+               + "\n"           
+               + "Introduce una opcion  \n")); 
+                
+        switch (opcion){
             case 1 : visualizar();
                 break;
             case 2 : buscarNum();
@@ -67,15 +54,29 @@ public class Ejercicio1 {
         }
            
         
-    }
+    } 
+         while (opcion != 10);
+        }
+
+    
     public static void visualizar(){
+        
+           
+        /*
+         ArrayList<float> valores = new ArrayList<float>();
+        
+          String valor = JOptionPane.showMessageDialog(null, "Introduce un valor"));
+          valores.add("valor");
+           */
     
     }
     public static void buscarNum(){
+        
+        JOptionPane.showMessageDialog(null, "Numeros negativos no validos");
     
     }
     public static void borrarNum(){
-    
+      
     }
     public static void array(){
     
